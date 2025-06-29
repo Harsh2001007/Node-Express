8d7ea372d10114613e5e2a9ba00db6414358745e
Harsh <hs5686584@gmail.com>
2025-06-29 14:12:42 +0530

    dynamic routing and ejs setup done

--- Revision/index.js
+++ Revision/index.js
@@ -1,26 +1,26 @@
-const { json } = require("body-parser");
-const { log } = require("console");
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use("/", function (req, res, next) {
-  console.log("i am other middleware");
-  next();
-});
-
-app.use("/profile", (req, res, next) => {
-  console.log("i reached before profile page server");
-  next();
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 
-app.get("/profile", (req, res) => {
-  res.send("i am your profile");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/", (req, res) => {
-  res.send("i am at homepage");
+app.get("/profile/:profilename", (req, res) => {
+  res.send(req.params.profilename);
+});
+
+app.get("/author/:authorname/:authorId", (req, res) => {
+  res.send(
+    `the author is ${req.params.authorname} and this doc is ${req.params.authorId}`
+  );
 });
 
 app.listen(5001, () => {
